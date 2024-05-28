1f884c922d1c5aa2b77a38384187c264cf19bf93
Bryce Lynn <bryce.lynn@nttdata.com>
2024-05-28 16:10:29 -0700

    in progress. adding tests

---
+++ tests/testimpl/test_impl.go
@@ -0,0 +1,24 @@
+package common
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/appmesh"
+
+	"github.com/launchbynttdata/lcaf-component-terratest/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDoesAppmeshRouteExist(t *testing.T, ctx types.TestContext) {
+	appmeshClient := appmesh.NewFromConfig(GetAWSConfig(t))
+
+}
+
+func GetAWSConfig(t *testing.T) (cfg aws.Config) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	require.NoErrorf(t, err, "unable to load SDK config, %v", err)
+	return cfg
+}
---
+++ tests/testimpl/types.go
@@ -0,0 +1,9 @@
+package common
+
+import (
+	"github.com/launchbynttdata/lcaf-component-terratest/types"
+)
+
+type ThisTFModuleConfig struct {
+	types.GenericTFModuleConfig
+}
