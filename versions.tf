cee4cdd6aebd8a69f7ca9994c4ad9275f081d215
Bryce Lynn <bryce.lynn@nttdata.com>
2024-06-20 14:39:31 -0700

    update aws provider version

--- versions.tf
+++ versions.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.57.0"
+      version = "~> 5.0"
     }
     random = {
       source  = "hashicorp/random"
